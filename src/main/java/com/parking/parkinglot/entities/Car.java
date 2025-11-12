22b3a418e551645bddb24fb3b45d7e5dda0dc53e
Sebiobo <oboroceanusebi@yahoo.com>
2025-11-12 16:42:54 +0200

    Created the entities

---
+++ src/main/java/com/parking/parkinglot/entities/Car.java
@@ -0,0 +1,55 @@
+package com.parking.parkinglot.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "cars")
+public class Car {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "parking_spot")
+    private String parkingSpot;
+
+    @Column(name = "lincense_plate")
+    private String lincensePlate;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "owner_id", nullable = false)
+    private User owner;
+
+    public User getOwner() {
+        return owner;
+    }
+
+    public void setOwner(User owner) {
+        this.owner = owner;
+    }
+
+    public String getParkingSpot() {
+        return parkingSpot;
+    }
+
+    public void setParkingSpot(String parkingSpot) {
+        this.parkingSpot = parkingSpot;
+    }
+
+    public String getLincensePlate() {
+        return lincensePlate;
+    }
+
+    public void setLincensePlate(String lincensePlate) {
+        this.lincensePlate = lincensePlate;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+}
--- src/main/webapp/WEB-INF/pages/menu.jsp
+++ src/main/webapp/WEB-INF/pages/menu.jsp
@@ -9,7 +9,11 @@
                  <ul class="navbar-nav me-auto mb-2 mb-md-0">
                      <li class="nav-item">
                          <a class="nav-link active" aria-current="page" href="${pageContext.request.contextPath}/about.jsp">About</a>
-                     </li> <li class="nav-item">
+                     </li>
+                     <li class="nav-item">
+                         <a class="nav-link active" aria-current="page" href="${pageContext.request.contextPath}/Cars">Cars</a>
+                     </li>
+                     <li class="nav-item">
                      <a class="nav-link" href="#">Link</a>
                  </li> <li class="nav-item">
                      <a class="nav-link disabled" aria-disabled="true">Disabled</a>
